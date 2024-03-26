529cc28b0497c2f32ea09e108a0f372b936acc4c
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-03-26 15:22:52 +0530

    transition updated"

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,45 @@
+import {motion} from 'framer-motion'
+
+const transitionVariants = {
+  initial: {
+    left: '100%',
+  },
+  animate: {
+    left: '100%',
+  },
+  exit: {
+    left: '100%',
+  },
+};
+
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div 
+        className='fixed top-0 bottom-0 right-full w-screen  h-screen z-30 bg-[#2e2257]'
+        variants={transitionVariants} 
+        intial='intial' 
+        animate='animate' 
+        exit='exit' 
+        transition={{delay:0.2,duration:1,ease:'easeInOut'}}
+      ></motion.div>
+      <motion.div 
+        className='fixed  top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
+        variants={transitionVariants} 
+        intial='intial' 
+        animate='animate' 
+        exit='exit' 
+        transition={{delay:0.4,duration:1,ease:'easeInOut'}}
+      ></motion.div>
+      <motion.div 
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
+        variants={transitionVariants} 
+        intial='intial' 
+        animate='animate' 
+        exit='exit' 
+        transition={{delay:0.6,duration:1,ease:'easeInOut'}}
+      ></motion.div>
     </>
   );
 };
