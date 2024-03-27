496e56baa96c1422312db0c22c6065844b5e566f
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-03-27 18:01:43 +0530

    about page done"

--- pages/about/index.js
+++ pages/about/index.js
@@ -97,6 +97,8 @@ import Circles from '../../components/Circles';
 import {motion} from 'framer-motion'
 import { fadeIn } from '../../variants';
 
+import CountUP from 'react-countup'
+
 const About = () => {
   const [index,setIndex]=useState(0)
   return (
@@ -109,13 +111,83 @@ const About = () => {
       exit='hidden'
       className="hidden xl:flex absolute bottom-0 -left-[370px]">
 
-      <Avatar/>
+      {/* <Avatar/> */}
     </motion.div>
     <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
       <div className="flex-1 flex flex-col justify-center">
-        text
+        <motion.h2 
+           variants={fadeIn('right',0.2)}
+           initial='hidden'
+           animate='show'
+           exit='hidden'
+          className='h2 text-5xl'>
+          Captivating <span className='text-accent'>stories</span>birth magnificent designs.
+        </motion.h2>
+        <motion.p 
+           variants={fadeIn('right',0.2)}
+           initial='hidden'
+           animate='show'
+           exit='hidden'
+          className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+          I am driven by the belief that technology should be accessible to all,
+          and I actively seek opportunities to collaborate, share knowledge, and mentor aspiring developers. 
+        </motion.p>
+        <motion.div 
+           variants={fadeIn('right',0.6)}
+           initial='hidden'
+           animate='show'
+           exit='hidden'
+          className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'>
+          <div className='flex flex-1 xl:gap-x-6' >
+            <div className="relative felx-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2' >
+                <CountUP start={0} end={1.5} duration={5}/>+
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-wp[100px]' >
+                years of experience
+              </div>
+            </div>
+            
+            <div className="relative felx-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2' >
+                <CountUP start={0} end={7} duration={5}/>+
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-wp[100px]' >
+                Happy Client
+              </div>
+            </div>
+            
+            <div className="relative felx-1 after:w-[1px] after:h-full
+            after:bg-white/10 after:absolute after:top-0 after:right-0 ">
+              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2' >
+                <CountUP start={0} end={24} duration={5}/>
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-wp[100px]' >
+                Hours of support
+              </div>
+            </div>
+            
+            <div className="relative felx-1 ">
+              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2' >
+                <CountUP start={0} end={8} duration={5}/>
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-wp[100px]' >
+                Projects
+              </div>
+            </div>
+
+          </div>
+        </motion.div>
       </div>
-      <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+
+      <motion.div 
+         variants={fadeIn('left',0.4)}
+         initial='hidden'
+         animate='show'
+         exit='hidden'
+        className="flex flex-col w-full items-center xl:max-w-[48%] h-[480px]">
         <div 
         className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
           {aboutData.map((item,itemIndex)=>{
@@ -160,7 +232,7 @@ const About = () => {
             )
           })}
         </div>
-      </div>
+      </motion.div>
     </div>
     
   </div>
